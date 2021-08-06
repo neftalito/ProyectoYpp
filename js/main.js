7a5d47cd317084b9195ddafac466655f8d93a231
Neftalí Toledo <toledoneftali@hotmail.com>
2021-08-06 14:02:08 -0300

    Barra de navegacion fijada y algunos errores solucionados

--- js/main.js
+++ js/main.js
@@ -3,6 +3,10 @@ var BotonTexto = document.getElementById("BotonTexto");
 var MenuContenido = document.getElementById("MenuContenido");
 var MenuScroll = document.getElementsByClassName("MenuScroll");
 var Pagina = document.getElementsByClassName("Pagina");
+var media = window.matchMedia("(min-width: 1020px)");
+var mediajs = window.matchMedia("(max-width: 648px)");
+var Informacion = document.getElementsByClassName("Informacion");
+
 function ToggleMenu(){
     if(Menu){
         BotonTexto.textContent = "Abrir";
@@ -17,4 +21,34 @@ function ToggleMenu(){
         Menu=true;
     }
 }
-ToggleMenu();
+function ToggleMenuCelu(){
+    if(Menu && mediajs.matches){
+        //Cerrado
+        BotonTexto.textContent = "Abrir";
+        MenuScroll[0].style.display = "none";
+        Pagina[0].style.display = "block";
+        Informacion[0].style.display = "Block";
+        Menu = false;
+    }
+    else{
+        //Abierto
+        BotonTexto.textContent = "Cerrar";
+        MenuScroll[0].style.display = "Flex";
+        Pagina[0].style.display = "Block";
+        Informacion[0].style.display = "none";
+        Menu=true;
+    }
+}
+
+if(media.matches){
+    ToggleMenu();
+}
+
+
+if(mediajs.matches){
+    let Botonmenu = document.getElementById("MenuBtn");
+    Botonmenu.firstElementChild.onclick = function(){ToggleMenuCelu()};
+    document.body.classList.add("Celular");
+    MenuScroll[0].classList.add("contenedor-centrado");
+    MenuScroll[0].classList.add("MenuScrollCelular");
+}
